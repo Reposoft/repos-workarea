25c7238bf50edd7e5bc93529005037b7e06be571
hsvallfors <hsvallfors>
2013-12-09 15:23:16 +0000

    Added a class for CmsItemLocks against local files.

---
+++ src/main/java/se/repos/cms/backend/filehead/LocalCmsItemLock.java
@@ -0,0 +1,45 @@
+package se.repos.cms.backend.filehead;
+
+import java.util.Date;
+
+import se.simonsoft.cms.item.CmsItemId;
+import se.simonsoft.cms.item.CmsItemLock;
+
+public class LocalCmsItemLock implements CmsItemLock {
+
+    @Override
+    public String getComment() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Date getDateCreation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Date getDateExpiration() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getOwner() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public CmsItemId getItemId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
