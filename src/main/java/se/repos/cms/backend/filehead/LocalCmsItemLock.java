d6975a456375db501d934b31d8f1a9471bb54e52
hsvallfors <hsvallfors>
2013-12-12 11:31:55 +0000

    Partially implemented CmsItemLock.

--- src/main/java/se/repos/cms/backend/filehead/LocalCmsItemLock.java
+++ src/main/java/se/repos/cms/backend/filehead/LocalCmsItemLock.java
@@ -1,45 +1,90 @@
 package se.repos.cms.backend.filehead;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Date;
 
+import javax.inject.Inject;
+
+import org.apache.commons.io.IOUtils;
+
+import se.repos.authproxy.ReposCurrentUser;
 import se.simonsoft.cms.item.CmsItemId;
 import se.simonsoft.cms.item.CmsItemLock;
+import se.simonsoft.cms.item.CmsItemPath;
+import se.simonsoft.cms.item.CmsRepository;
+import se.simonsoft.cms.item.impl.CmsItemIdUrl;
 
 public class LocalCmsItemLock implements CmsItemLock {
+    private CmsItemPath lockPath;
+
+    private CmsRepository repository;
+    private ReposCurrentUser currentUser;
+
+    @Inject
+    public void setRepository(CmsRepository repository) {
+        this.repository = repository;
+    }
+
+    @Inject
+    public void setReposCurrentUser(ReposCurrentUser currentUser) {
+        this.currentUser = currentUser;
+    }
+    
+    private LocalCmsItemLock(CmsItemPath lockPath) {
+        this.lockPath = lockPath;
+    }
+    
+    public static LocalCmsItemLock getLocalLock(CmsItemPath lockedItem, String lockComment) {
+        Date creationDate = new Date();
+        // TODO Create lock file, write lock comment and date to it.
+        return null;
+    }
+    
+    private File getLockFile() {
+        String filePath = this.repository.getPath() + this.lockPath.getPath();
+        return new File(filePath);
+    }
 
     @Override
     public String getComment() {
-        // TODO Auto-generated method stub
-        return null;
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(getLockFile());
+            return IOUtils.toString(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getCause());
+        } finally {
+            IOUtils.closeQuietly(fis);
+        }        
     }
 
     @Override
     public Date getDateCreation() {
-        // TODO Auto-generated method stub
+        // TODO Read lock file, exctract creation date from it.
         return null;
     }
 
     @Override
     public Date getDateExpiration() {
-        // TODO Auto-generated method stub
+        // There is no defined expiration Date for these locks.
         return null;
     }
 
     @Override
     public String getToken() {
-        // TODO Auto-generated method stub
+        // TODO What is the lock tocken?
         return null;
     }
 
     @Override
     public String getOwner() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.currentUser.getUsername();
     }
 
     @Override
     public CmsItemId getItemId() {
-        // TODO Auto-generated method stub
-        return null;
+        return new CmsItemIdUrl(this.repository, this.lockPath);
     }
 }
